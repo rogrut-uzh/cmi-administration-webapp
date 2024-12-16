3936728d58fd077bc308e32094d10d3397bd6df7
Roger Rutishauser (rogrut-adm) <rogrut-adm@d.uzh.ch>
2024-12-16 16:47:18 +0100

    update

--- cmi-administration-webapp/app.py
+++ cmi-administration-webapp/app.py
@@ -57,4 +57,4 @@ def run_script():
 
 # Run the Flask application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=false)
+    app.run(host='0.0.0.0', port=5000, debug=False)
