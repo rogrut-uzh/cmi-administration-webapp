1da4091ae90f4a06d204ec3f4e0e5f0e46148005
Roger Rutishauser <roger.rutishauser@uzh.ch>
2025-04-16 10:14:00 +0200

    start stop services

--- pwsh/cmi-databases.ps1
+++ pwsh/cmi-databases.ps1
@@ -10,6 +10,7 @@ $env:NO_PROXY = "localhost,127.0.0.1,::1"
 $env:HTTP_PROXY = "http://zoneproxy.zi.uzh.ch:8080"
 $env:HTTPS_PROXY = "http://zoneproxy.zi.uzh.ch:8080"
 
+$ApiRoot = "http://localhost:5001/api/data"
 
 function Get-CMI-Config-Data {
     param (
@@ -42,14 +43,15 @@ function Get-CMI-Config-Data {
 
 function CreateDbList {
     param (
-        [string]$Env
+        [string]$Env,
+        [string]$ApiRoot
     )
     $dbs = @()
     $jsonData = @()
     
     try {
-        $jsonData += Get-CMI-Config-Data -u "http://localhost:5001/api/data/cmi/${Env}"
-        $jsonData += Get-CMI-Config-Data -u "http://localhost:5001/api/data/ais/${Env}"
+        $jsonData += Get-CMI-Config-Data -u "${ApiRoot}/cmi/${Env}"
+        $jsonData += Get-CMI-Config-Data -u "${ApiRoot}/ais/${Env}"
         if (($jsonData | Measure-Object).count -lt 1) {
             Write-Output (@{
                 error = "Keine Daten gefunden."
@@ -101,6 +103,8 @@ function CreateDbBackup {
         try {
             #Write-Output "STARTE BACKUP"
             Backup-SqlDatabase -ServerInstance "$DbHost" -Database $Db -BackupFile "${backupPath}${backupFilename}" -CopyOnly -Initialize -Checksum
+            #Backup-SqlDatabase -ServerInstance "$DbHost" -Database $Db -BackupFile "${backupPath}${backupFilename}" -CopyOnly -Initialize -Checksum -PercentCompleteNotification 10
+            
             Write-Output "SUCCESS"
             exit 0
         }
@@ -112,13 +116,47 @@ function CreateDbBackup {
 }
 
 
+function CmiAppService {
+    param (
+        [string]$Db,
+        [string]$Action,
+        [string]$ApiRoot
+    )
+    
+    # get the CMI service name and the hostname where the CMI service is running
+    try {
+        $jsonData = Get-CMI-Config-Data -u "${ApiRoot}?database/name=${Db}"
+        
+        if (($jsonData | Measure-Object).count -lt 1) {
+            Write-Output (@{
+                error = "Keine Daten gefunden."
+            } | ConvertTo-Json -Compress)
+            exit 1
+        }
+        $jsonData = @($jsonData)
+        
+        $Service = $jsonData.app.servicename._text
+        $Hostname = $jsonData.app.host._text
+    }
+    catch {
+        $Hostname = ""
+        $Service = "Error: $($_.Exception.Message)"
+    }
+    
+    # start or stop the service
+    & "$PSScriptRoot\cmi-control-single-service.ps1" -Service $Service -Action $Action -Hostname $Hostname
+    return $LASTEXITCODE
+    
+}
+
+
 # run
 if ($Job -eq "list") {
     if ($Env -eq $Null) {
         Write-Output (@{ error = "No environment defined" } | ConvertTo-Json -Compress)
         exit 1
     }
-    Write-Output (CreateDbList -Env $Env)
+    Write-Output (CreateDbList -Env $Env -ApiRoot $ApiRoot)
     
 } elseif ($Job -eq "backup") {
     if ($Db -eq $Null) {
@@ -129,7 +167,14 @@ if ($Job -eq "list") {
         Write-Output (@{ error = "No database host defined" } | ConvertTo-Json -Compress)
         exit 1
     }
-    CreateDbBackup -Db $Db -DbHost $DbHost
+    
+    $CmiAppServiceStatus = CmiAppService -Db $Db -Action "stop" -ApiRoot $ApiRoot
+    if ($CmiAppServiceStatus -eq 0) {
+        CreateDbBackup -Db $Db -DbHost $DbHost
+        if ($LASTEXITCODE -eq 0) {
+            $CmiAppServiceStatus = CmiAppService -Db $Db -Action "start" -ApiRoot $ApiRoot
+        }
+    }
     
 } else {
     Write-Output (@{ error = "No valid job defined" } | ConvertTo-Json -Compress)
