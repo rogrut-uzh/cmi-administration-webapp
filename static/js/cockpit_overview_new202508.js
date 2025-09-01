ac768ac994fa9a303560b07532115dd414ea33cf
Roger Rutishauser <roger.rutishauser@uzh.ch>
2025-09-01 16:26:20 +0200

    new cockpit view

--- static/js/cockpit_overview_new202508.js
+++ static/js/cockpit_overview_new202508.js
@@ -80,7 +80,7 @@ function populateTable(data) {
 
         const mandantCell = document.createElement("td");
         mandantCell.classList.add(tdclass);
-        mandantCell.textContent = item.mand._text || "";
+        mandantCell.textContent = item?.mand?._text ?? item?.mand ?? "";
         row.appendChild(mandantCell);
 
         const hostCell = document.createElement("td");
